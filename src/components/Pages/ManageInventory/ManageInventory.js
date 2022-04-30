2facddc609ceaf2b87ee2dddf0441e0aea75bf1e
razuahmedjoy <rjjoybzs420@gmail.com>
2022-05-01 02:43:42 +0600

    delete api and button click done

--- src/components/Pages/ManageInventory/ManageInventory.js
+++ src/components/Pages/ManageInventory/ManageInventory.js
@@ -2,13 +2,16 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 const ManageInventory = () => {
 
-    const [inventories,setInventories] = useState([]);
-    const [perPage,setPerPage] = useState(10);
-    const [page,setPage] = useState(0)
-    const [totalPage,setTotalPage] = useState(0)
+    const [inventories, setInventories] = useState([]);
+    const [perPage, setPerPage] = useState(10);
+    const [page, setPage] = useState(0)
+    const [totalPage, setTotalPage] = useState(0);
+
+    const [loading, setLoading] = useState(false)
 
 
     useEffect(() => {
@@ -16,14 +19,112 @@ const ManageInventory = () => {
         const getTotalCount = async () => {
 
             const res = await axios.get(`http://localhost:8000/inventories?count=True`);
-            const {totalCount} = res.data;
+            const { totalCount } = res.data;
             const pageCount = Math.ceil(totalCount / perPage);
             setTotalPage(pageCount);
 
         }
 
         getTotalCount();
-    },[])
+
+    }, [])
+
+    useEffect(() => {
+        setLoading(true)
+        const getInventories = async () => {
+            const res = await axios.get(`http://localhost:8000/inventories?page=${page}`);
+            const { data } = res;
+
+            setInventories(data);
+
+            setLoading(false);
+        }
+        getInventories();
+
+    }, [page])
+
+
+    const Spinner = () => {
+        return (
+            <div className="flex justify-center items-center">
+                <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            </div>
+        )
+    }
+
+    const deleteItem = async (productId) => {
+        const res = await axios.post(`http://localhost:8000/delete`,{id:productId})
+        const {data} = res;
+        if (data.deletedCount === 1){
+            const newInventoryList = inventories.filter(item => item._id !== productId)
+            setInventories(newInventoryList);
+            toast("Item deleted")
+        }
+    }
+
+    const InventoryTableData = () => {
+        return (<>
+            <table className="min-w-full table-auto">
+                <thead className="border-b">
+                    <tr className="bg-gray-900 text-white">
+
+                        <th width="100px" scope="col" className="text-sm font-medium px-6 py-4 text-left">
+                            Image
+                        </th>
+                        <th scope="col" className="text-sm font-medium px-6 py-4 text-left">
+                            Name
+                        </th>
+                        <th scope="col" className="text-sm font-medium px-6 py-4 text-left">
+                            Quantity
+                        </th>
+                        <th scope="col" className="text-sm font-medium px-6 py-4 text-left">
+                            Price
+                        </th>
+                        <th scope="col" className="text-sm font-medium px-6 py-4 text-left">
+                            Supplier
+                        </th>
+                        <th scope="col" className="text-sm font-medium px-6 py-4 text-left">
+                            Action
+                        </th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {inventories.map((item) =>
+                        <tr key={item._id} className="border-b">
+
+                            <td className="text-sm text-gray-900 font-light px-6 py-4">
+                                <div className="flex rounded-full justify center items-center border-2 w-[70px]">
+                                    <img className="rounded-full object-cover" src={item.image} alt="" />
+                                </div>
+                            </td>
+                            <td className="text-sm text-gray-900 font-light px-6 py-4">
+                                {item.name}
+                            </td>
+                            <td className="text-sm text-gray-900 font-light px-6 py-4">
+                                {item.quantity}
+                            </td>
+                            <td className="text-sm text-gray-900 font-light px-6 py-4">
+                                ${item.price}
+                            </td>
+                            <td className="text-sm text-gray-900 font-light px-6 py-4">
+                                {item.supllier_name}
+                            </td>
+                            <td className="text-sm text-gray-900 font-light px-6 py-4">
+                                <button onClick={() => deleteItem(item._id)} className="btn border-[1px] border-red-500 py-1 text-red-500 px-5 rounded-full hover:bg-red-500 hover:text-white duration-300"><FontAwesomeIcon icon={faTrash} /></button>
+                            </td>
+
+                        </tr>
+                    )
+                    }
+                </tbody>
+            </table>
+        </>
+
+        )
+    }
 
     return (
         <div>
@@ -37,66 +138,17 @@ const ManageInventory = () => {
                     <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
                         <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
                             <div className="overflow-x-auto">
-                                <table className="min-w-full table-auto">
-                                    <thead className="border-b">
-                                        <tr className="bg-gray-900 text-white">
 
-                                            <th width="100px" scope="col" className="text-sm font-medium px-6 py-4 text-left">
-                                                Image
-                                            </th>
-                                            <th scope="col" className="text-sm font-medium px-6 py-4 text-left">
-                                                Name
-                                            </th>
-                                            <th scope="col" className="text-sm font-medium px-6 py-4 text-left">
-                                                Quantity
-                                            </th>
-                                            <th scope="col" className="text-sm font-medium px-6 py-4 text-left">
-                                                Price
-                                            </th>
-                                            <th scope="col" className="text-sm font-medium px-6 py-4 text-left">
-                                                Supplier
-                                            </th>
-                                            <th scope="col" className="text-sm font-medium px-6 py-4 text-left">
-                                                Action
-                                            </th>
 
-                                        </tr>
-                                    </thead>
-                                    <tbody>
+                                {loading ? <Spinner /> : <InventoryTableData />}
 
-                                        <tr className="border-b">
 
-                                            <td className="text-sm text-gray-900 font-light px-6 py-4">
-                                                <div className="flex rounded-full justify center items-center border-2 w-[70px]">
-                                                    <img className="rounded-full object-cover" src="https://motorbike.autoshowroom.co/wp-content/uploads/2017/12/Kawasaki-Z900.jpg" alt="" />
-                                                </div>
-                                            </td>
-                                            <td className="text-sm text-gray-900 font-light px-6 py-4">
-                                                Here is the name of the Honda
-                                            </td>
-                                            <td className="text-sm text-gray-900 font-light px-6 py-4">
-                                                500
-                                            </td>
-                                            <td className="text-sm text-gray-900 font-light px-6 py-4">
-                                                $450
-                                            </td>
-                                            <td className="text-sm text-gray-900 font-light px-6 py-4">
-                                                Supplier
-                                            </td>
-                                            <td className="text-sm text-gray-900 font-light px-6 py-4">
-                                                <button className="btn border-[1px] border-red-500 py-1 text-red-500 px-5 rounded-full hover:bg-red-500 hover:text-white duration-300"><FontAwesomeIcon icon={faTrash} /></button>
-                                            </td>
-
-                                        </tr>
-                            
-                                    </tbody>
-                                </table>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
