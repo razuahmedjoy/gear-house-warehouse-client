9aaba3eda57351a4bad6c70810f5f61b7e26591f
razuahmedjoy <rjjoybzs420@gmail.com>
2022-05-01 02:07:32 +0600

    got total items and setted totalPagecount

--- src/components/Pages/ManageInventory/ManageInventory.js
+++ src/components/Pages/ManageInventory/ManageInventory.js
@@ -5,18 +5,24 @@ import React, { useEffect, useState } from 'react';
 
 const ManageInventory = () => {
 
-    const [items,setItems] = useState([]);
+    const [inventories,setInventories] = useState([]);
+    const [perPage,setPerPage] = useState(10);
+    const [page,setPage] = useState(0)
+    const [totalPage,setTotalPage] = useState(0)
 
 
     useEffect(() => {
 
-        const getItems = async () => {
+        const getTotalCount = async () => {
 
-            // const res = await axios.get(`http://localhost:8000/inventories?`)
+            const res = await axios.get(`http://localhost:8000/inventories?count=True`);
+            const {totalCount} = res.data;
+            const pageCount = Math.ceil(totalCount / perPage);
+            setTotalPage(pageCount);
 
         }
 
-        getItems();
+        getTotalCount();
     },[])
 
     return (
@@ -27,30 +33,30 @@ const ManageInventory = () => {
             </div>
 
             <div className="inventories w-full px-1 md:px-10">
-                <div class="flex flex-col">
-                    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                            <div class="overflow-x-auto">
-                                <table class="min-w-full table-auto">
-                                    <thead class="border-b">
+                <div className="flex flex-col">
+                    <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                            <div className="overflow-x-auto">
+                                <table className="min-w-full table-auto">
+                                    <thead className="border-b">
                                         <tr className="bg-gray-900 text-white">
 
-                                            <th width="100px" scope="col" class="text-sm font-medium px-6 py-4 text-left">
+                                            <th width="100px" scope="col" className="text-sm font-medium px-6 py-4 text-left">
                                                 Image
                                             </th>
-                                            <th scope="col" class="text-sm font-medium px-6 py-4 text-left">
+                                            <th scope="col" className="text-sm font-medium px-6 py-4 text-left">
                                                 Name
                                             </th>
-                                            <th scope="col" class="text-sm font-medium px-6 py-4 text-left">
+                                            <th scope="col" className="text-sm font-medium px-6 py-4 text-left">
                                                 Quantity
                                             </th>
-                                            <th scope="col" class="text-sm font-medium px-6 py-4 text-left">
+                                            <th scope="col" className="text-sm font-medium px-6 py-4 text-left">
                                                 Price
                                             </th>
-                                            <th scope="col" class="text-sm font-medium px-6 py-4 text-left">
+                                            <th scope="col" className="text-sm font-medium px-6 py-4 text-left">
                                                 Supplier
                                             </th>
-                                            <th scope="col" class="text-sm font-medium px-6 py-4 text-left">
+                                            <th scope="col" className="text-sm font-medium px-6 py-4 text-left">
                                                 Action
                                             </th>
 
@@ -58,26 +64,26 @@ const ManageInventory = () => {
                                     </thead>
                                     <tbody>
 
-                                        <tr class="border-b">
+                                        <tr className="border-b">
 
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4">
-                                                <div class="flex rounded-full justify center items-center border-2 w-[70px]">
+                                            <td className="text-sm text-gray-900 font-light px-6 py-4">
+                                                <div className="flex rounded-full justify center items-center border-2 w-[70px]">
                                                     <img className="rounded-full object-cover" src="https://motorbike.autoshowroom.co/wp-content/uploads/2017/12/Kawasaki-Z900.jpg" alt="" />
                                                 </div>
                                             </td>
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4">
+                                            <td className="text-sm text-gray-900 font-light px-6 py-4">
                                                 Here is the name of the Honda
                                             </td>
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4">
+                                            <td className="text-sm text-gray-900 font-light px-6 py-4">
                                                 500
                                             </td>
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4">
+                                            <td className="text-sm text-gray-900 font-light px-6 py-4">
                                                 $450
                                             </td>
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4">
+                                            <td className="text-sm text-gray-900 font-light px-6 py-4">
                                                 Supplier
                                             </td>
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4">
+                                            <td className="text-sm text-gray-900 font-light px-6 py-4">
                                                 <button className="btn border-[1px] border-red-500 py-1 text-red-500 px-5 rounded-full hover:bg-red-500 hover:text-white duration-300"><FontAwesomeIcon icon={faTrash} /></button>
                                             </td>
 
